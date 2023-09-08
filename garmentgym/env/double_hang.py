bf80691e3f5340d25300f590564ecc44b29a7647
luhr2003 <luhr200342@163.com>
2023-09-08 14:27:39 +0800

    hang env adjust

--- garmentgym/env/double_hang.py
+++ garmentgym/env/double_hang.py
@@ -27,7 +27,7 @@ from garmentgym.garmentgym.utils.basic_utils import make_dir
 task_config = {"task_config": {
     'observation_mode': 'cam_rgb',
     'action_mode': 'pickerpickplace',
-    'num_picker': 1,
+    'num_picker': 2,
     'render': True,
     'headless': False,
     'horizon': 100,
@@ -39,7 +39,7 @@ from garmentgym.garmentgym.base.record import task_info
 
 
 
-class HangEnv(ClothesHangEnv):
+class DoubleHangEnv(ClothesHangEnv):
     def __init__(self,mesh_category_path:str,gui=True,store_path="./",id=-1):
         self.config=Config(task_config)
         self.id=id
@@ -126,62 +126,173 @@ class HangEnv(ClothesHangEnv):
     
     def pick(self,p1,p2):
         '''a demo funciton to show clothes can hang up sucessfully'''
-        #self.set_grasp(False)
+        self.set_grasp(False)
         self.movep([p1], speed=2e-2)
         self.set_grasp(True)
-        env.update_camera(0)
-        
-        
         self.movep([p2], speed=2e-2)
         self.set_grasp(False)
         self.hide_end_effectors()
     
+
+    def two_pick_and_down(self, p1_s,p1_m ,p1_e, p2_s,p2_m,p2_e,lift_height=0.5,down_height=0.03):
+    # prepare primitive params
+        pick_pos1, mid_pos1,place_pos1 = p1_s.copy(),p1_m.copy(), p1_e.copy()
+        pick_pos2, mid_pos2,place_pos2 = p2_s.copy(),p2_m.copy(), p2_e.copy()
+
+        pick_pos1[1] += down_height
+        mid_pos1[1]+=down_height+0.04
+        place_pos1[1] += 0.03 + 0.05
+        pick_pos2[1] += 0.03
+        mid_pos2[1]+=0.03
+        place_pos2[1] += 0.03 + 0.05
+
+        prepick_pos1 = pick_pos1.copy()
+        prepick_pos1[1] = lift_height
+        premid_pos1 = mid_pos1.copy()
+        premid_pos1[1] = lift_height
+        preplace_pos1 = place_pos1.copy()
+        preplace_pos1[1] = lift_height
+        prepick_pos2 = pick_pos2.copy()
+        prepick_pos2[1] = lift_height
+        premid_pos2 = mid_pos2.copy()
+        premid_pos2[1]=lift_height
+        preplace_pos2 = place_pos2.copy()
+        preplace_pos2[1] = lift_height
+
+        # execute action
+        self.set_grasp([False, False])
+        self.two_movep([prepick_pos1, prepick_pos2], speed=1e-2)  # 修改此处
+        self.two_movep([pick_pos1, pick_pos2], speed=1e-2)  # 修改此处
+        self.set_grasp([True, True])
+        self.two_movep([prepick_pos1, prepick_pos2], speed=1e-2)  # 修改此处
+        self.two_movep([premid_pos1,premid_pos2], speed=1e-2)  # 修改此处
+        self.two_movep([mid_pos1,mid_pos2], speed=1e-2)  # 修改此处
+        self.two_movep([premid_pos1,premid_pos2], speed=1e-2)  # 修改此处
         
-    def middle_step(self,p1):
-        self.set_grasp(True)
-        self.update_camera(0)
-        self.movep([p1],speed=2e-2)
-        
-        
+        self.two_movep([preplace_pos1, preplace_pos2], speed=1e-2)  # 修改此处
+        self.two_movep([place_pos1, place_pos2], speed=1e-2)  # 修改此处
+        self.set_grasp([False, False])
+        self.two_movep([preplace_pos1, preplace_pos2], speed=1e-2)  # 修改此处
+        self.two_hide_end_effectors()
     
-    def start_step(self,p1):
-        self.set_grasp(False)
-        self.movep([p1],speed=2e-2)
-        self.set_grasp(True)
-        p1_high=p1.copy()
-        p1_high[1]+=0.2
-        self.movep([p1_high],speed=2e-2)
-        
-    def final_step(self,p1):
-        self.set_grasp(True)
-        self.update_camera(1)
-        self.movep([p1],speed=2e-2)
-        self.set_grasp(False)
-        self.hide_end_effectors()
+    
+    
+    def two_pick_change_nodown(
+        self, p1, p2,p3 ,p4,p5,p6,lift_height=0.3):
+        # prepare primitive params
+        pick_pos1, mid_pos1,place_pos1 = p1.copy(), p2.copy(),p3.copy()
+        pick_pos2, mid_pos2,place_pos2 = p4.copy(), p5.copy(),p6.copy()
+        pick_pos1[1] -= 0.04
+        place_pos1[1] += 0.03 + 0.05
+        mid_pos1[1] += 0.03 + 0.05
+        pick_pos2[1] -= 0.04
+        place_pos2[1] += 0.03 + 0.05
+        mid_pos2[1] += 0.03 + 0.05
 
-    def hang_trajectory(self,p1):
+        prepick_pos1 = pick_pos1.copy()
+        prepick_pos1[1] = lift_height
+        premid_pos1 = mid_pos1.copy()
+        premid_pos1[1] = lift_height
+        preplace_pos1 = place_pos1.copy()
+        preplace_pos1[1] = lift_height
+        
+        prepick_pos2 = pick_pos2.copy()
+        prepick_pos2[1] = lift_height
+        premid_pos2 = mid_pos2.copy()
+        premid_pos2[1] = lift_height
+        preplace_pos2 = place_pos2.copy()
+        preplace_pos2[1] = lift_height
+
+        # execute action
         self.set_grasp(False)
-        self.movep([p1],speed=2e-2)
+        self.two_movep([prepick_pos1,prepick_pos2], speed=8e-2)
+        self.two_movep([pick_pos1,pick_pos2], speed=6e-2)
         self.set_grasp(True)
-        p1_high=p1.copy()
-        p1_high[1]+=0.2
-        self.movep([p1_high],speed=2e-2)
-        self.set_grasp(True)
-        self.update_camera(0)
-        p1=[0.65,1.5,-0.5]
-        self.movep([p1],speed=2e-2)
-        self.set_grasp(True)
-        self.update_camera(1)
-        p1=[0.7,1.2,-0.64]
-        self.movep([p1],speed=2e-2)
+        self.two_movep([prepick_pos1,prepick_pos2], speed=1e-2)
+        self.two_movep([premid_pos1,premid_pos2], speed=2e-2)
+
+        self.two_movep([preplace_pos1,preplace_pos2], speed=1e-2)
+        self.two_movep([place_pos1,place_pos2], speed=2e-2)
         self.set_grasp(False)
-        self.hide_end_effectors()
+        self.two_movep([preplace_pos1,preplace_pos2], speed=8e-2)
+        self.two_hide_end_effectors()
+
+    def two_pick_and_place_primitive(self, p1_s, p1_e, p2_s,p2_e,lift_height=0.5,down_height=0.03):
+    # prepare primitive params
+        pick_pos1, place_pos1 = p1_s.copy(), p1_e.copy()
+        pick_pos2, place_pos2 = p2_s.copy(), p2_e.copy()
+
+        pick_pos1[1] += down_height
+        place_pos1[1] += 0.03 + 0.05
+        pick_pos2[1] += down_height
+        place_pos2[1] += 0.03 + 0.05
+
+        prepick_pos1 = pick_pos1.copy()
+        prepick_pos1[1] = lift_height
+        preplace_pos1 = place_pos1.copy()
+        preplace_pos1[1] = lift_height
+        prepick_pos2 = pick_pos2.copy()
+        prepick_pos2[1] = lift_height
+        preplace_pos2 = place_pos2.copy()
+        preplace_pos2[1] = lift_height
+
+        # execute action
+        self.set_grasp([False, False])
+        self.two_movep([prepick_pos1, prepick_pos2], speed=8e-2)  # 修改此处
+        self.two_movep([pick_pos1, pick_pos2], speed=8e-2)  # 修改此处
+        self.set_grasp([True, True])
+        self.two_movep([prepick_pos1, prepick_pos2], speed=1e-2)  # 修改此处
+        self.two_movep([preplace_pos1, preplace_pos2], speed=1e-2)  # 修改此处
+        self.two_movep([place_pos1, place_pos2], speed=1e-2)  # 修改此处
+        self.set_grasp([False, False])
+        self.two_movep([preplace_pos1, preplace_pos2], speed=1e-2)  # 修改此处
+        self.two_hide_end_effectors()
         
 
+    
+    
+    def two_movep(self, pos, speed=None, limit=1000, min_steps=None, eps=1e-4):
+        if speed is None:
+            speed = 0.08
+        print(pos)
+        target_pos = np.array(pos)
+        for step in range(limit):
+            curr_pos = self.action_tool._get_pos()[0]
+            print("curr_pos",curr_pos)  
+            print("targer_pos",target_pos)
+            deltas = [(targ - curr)
+                      for targ, curr in zip(target_pos, curr_pos)]
+            dists = [np.linalg.norm(delta) for delta in deltas]
+            if all([dist < eps for dist in dists]) and\
+                    (min_steps is None or step > min_steps):
+                return
+            action = []
+            
+            for targ, curr in zip(target_pos[:-1], curr_pos):  # 去掉目标位置的最后一个元素
+                delta = targ - curr  # 计算位置差值
+                dist = np.linalg.norm(delta)
+            
+            
+            
+            for targ, curr, delta, dist, gs in zip(
+                    target_pos, curr_pos, deltas, dists, self.grasp_states):
+                if dist < speed:
+                    action.extend([*targ, float(gs)])
+                else:
+                    delta = delta/dist
+                    action.extend([*(curr+delta*speed), float(gs)])
+            action = np.array(action)
+            self.action_tool.step(action)
+
+
+        raise MoveJointsException
     
     
     def hide_end_effectors(self):
         self.movep([[0.5, 0.5, -1]], speed=5e-2)
+        
+    def two_hide_end_effectors(self):
+        self.two_movep([[0.5, 0.5, -1],[0.5,0.5,-1]], speed=5e-2)
         
 
     def set_grasp(self, grasp):
@@ -313,39 +424,21 @@ class HangEnv(ClothesHangEnv):
             self.pick_and_change_route(arg[0],arg[1],arg[2])
         elif function=="pick_change_nodown":
             self.pick_change_nodown(arg[0],arg[1],arg[2])
-        elif function=="start_step":
-            self.start_step(arg[0])
-        elif function=="middle_step":
-            self.middle_step(arg[0])
-        elif function=="final_step":
-            self.final_step(arg[0])
-        elif function=="hang_trajectory":
-            self.hang_trajectory(arg[0])
 
-if __name__=="__main__":
+if __name__=="__main__":  
     #change mesh_category path to your own path
     #change id to demo shirt id
-    env=HangEnv(mesh_category_path="/home/luhr/correspondence/softgym_cloth/garmentgym/cloth3d/train",gui=True,store_path="./",id="00044")
+    env=DoubleHangEnv(mesh_category_path="/home/luhr/correspondence/softgym_cloth/garmentgym/cloth3d/train",gui=True,store_path="./",id="00044")
     env.update_camera(1)
     for j in range(100):
         pyflex.step()
         pyflex.render()
         
-    
-    
-    flat_pos=pyflex.get_positions().reshape(-1,4)
-    
-    middle_shoulder =flat_pos[env.clothes.right_shoulder][:3]
-    middle_shoulder[0]=(flat_pos[env.clothes.left_shoulder][0]+middle_shoulder[0])/2
-    middle_shoulder[2]+=0.05
-       
-    # env.update_camera(1)
-    # env.start_step(middle_shoulder)
-    # env.update_camera(0)
-    # env.middle_step([0.65,1.5,-0.5])
-    # env.final_step([0.7,1.2,-0.64])
-   # env.pick(middle_shoulder,[0.8,1.5,-0.8])
-    env.hang_trajectory(middle_shoulder)
+        
+    flat_pos=pyflex.get_positions().reshape(-1,4)[:,:3]
+    env.two_pick_and_place_primitive(flat_pos[env.clothes.left_shoulder],flat_pos[env.clothes.right_shoulder],[0.6,1.5,-0.5],[0.7,1.5,-0.5])
+    # env.two_pick_change_nodown([1,1,1],[0.5,1,1],[1,2,3],[1,1,2],[-0.2,1,0.4],[1,2,0.6])
+    #env.pick([0,0,0],[0.8,1.5,-0.8])
         
     
     
