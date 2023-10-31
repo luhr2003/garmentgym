e4a65e65cf7c8cd0efc02254e57e3254abb53b7b
luhr2003 <luhr200342@163.com>
2023-10-31 19:26:47 +0800

    wyy_try_server

--- garmentgym/env/fling.py
+++ garmentgym/env/fling.py
@@ -101,8 +101,7 @@ class FlingEnv(ClothesEnv):
         return self.info
     
     def throw_down(self):
-        self.two_pick_and_place_primitive([0,0,0],[0,2,0],[0.5,0.5,-1],[0.5,0.5,-1],lift_height=2)
-        self.hide_end_effectors()
+        self.two_pick_and_place_primitive([0,0,0],[0,2,0],[0.5,0.5,-1],[0.5,0.5,-1])
     
     
     
